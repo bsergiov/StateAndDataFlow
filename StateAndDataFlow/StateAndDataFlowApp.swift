7fe8f843dc62848142de2084e816115b748b92f0
bsergiov <81264661+bsergiov@users.noreply.github.com>
2022-02-22 11:18:33 +0200

    Initial Commit

---
+++ StateAndDataFlow/StateAndDataFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateAndDataFlowApp.swift
+//  StateAndDataFlow
+//
+//  Created by BSergio on 22.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct StateAndDataFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
